6254f1707ded51ad4c0b4f3975a371760080c22c
chauyiiiiii <113869665+chauyiiiiii@users.noreply.github.com>
2022-09-19 10:04:22 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,108 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . # . # .
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        # # . # .
+        # . # . .
+        # # . # .
+        # . # . .
+        # # . # .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # # . . .
+        # # # . .
+        # # . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . # . # #
+        . . # . #
+        . # . # #
+        . . # . #
+        . # . # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . . . # #
+        . . # # #
+        . . . # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        . . . # #
+        `)
+})
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
